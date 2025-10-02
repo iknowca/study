10621be2b5f8c42def814f6c9dfae7bb6b9e8275
wkkim <wkkim@raresoft.co.kr>
2025-10-02 11:02:51 +0900

    javawebdevworkbook: Todo 관련 DTO, Service, 컨트롤러, JSP 페이지 구현 및 목록/상세 조회 기능 추가

--- web/javawebdevworkbook/w1/src/main/java/org/zerock/w1/todo/TodoReadController.java
+++ web/javawebdevworkbook/w1/src/main/java/org/zerock/w1/todo/TodoReadController.java
@@ -6,17 +6,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(name = "todolistController", urlPatterns = "/todo/list")
-public class TodoListController extends HttpServlet {
+@WebServlet(name = "todoReadContoller", urlPatterns = "/todo/read")
+public class TodoReadController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("/todo/list");
+        System.out.println("/todo/read");
 
-        List<TodoDTO> list = TodoService.INSTANCE.getList();
-        req.setAttribute("list", list);
-        req.getRequestDispatcher("/WEB-INF/todo/list.jsp")
-                .forward(req, resp);
+        Long tno = Long.parseLong(req.getParameter("tno"));
+        TodoDTO dto = TodoService.INSTANCE.get(tno);
+        req.setAttribute("dto", dto);
+        req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req, resp);
     }
 }
