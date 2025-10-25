05b87456cae8dd31d2b03cd56822f282f5c04727
iknow <iknowca@gmail.com>
2025-10-26 01:32:42 +0900

    rustguide: [ch20] 멀티스레드 웹 서버 구현

---
+++ language/rust/rustguide/ch20/muti_thread_web_server/src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+use muti_thread_web_server::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+    println!("Listening on 127.0.0.1:7878");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        })
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, file_name) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => {
+            ("HTTP/1.1 404 Not Found", "404.html")
+        }
+    };
+
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len();
+    let response = format!("{}Content-Length: {}\r\n\r\n{}", status_line, length, contents);
+    stream.write_all(response.as_bytes()).unwrap();
+}
