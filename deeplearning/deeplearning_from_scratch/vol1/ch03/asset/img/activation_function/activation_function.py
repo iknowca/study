87a677a08c411e16c5f2fa905164c85b91d19de4
iknow <iknowca@gmail.com>
2025-10-30 23:51:05 +0900

    deeplearning/deeplearning_from_scratch/vol1: [ch03] 활성화함수

--- deeplearning/deeplearning_from_scratch/vol1/ch03/asset/img/activation_function/activation_function.py
+++ deeplearning/deeplearning_from_scratch/vol1/ch03/asset/img/activation_function/activation_function.py
@@ -6,7 +6,7 @@ sys.path.append(str(ROOT))
 import env.light_theme
 # manim -p -qm -s activation_function.py
 
-class Perceptron(Scene):
+class ActivationFunction(Scene):
     def construct(self):
         title = Tex(r"Perceptron", color=BLACK).scale(2).to_edge(UP)
         self.add(title)
@@ -38,10 +38,16 @@ class Perceptron(Scene):
         arrow_labels.add(arrow0_label, arrow1_label, arrow2_label)
         arrow_labels.shift(UP*0.3)
 
-        y_label = Tex(r"$y$", color=GREEN).move_to(y.get_center())
+        a_circle = Circle(radius=0.3, color=GREEN).move_to(y.get_center()).shift(LEFT*0.7)
+        y_circle = Circle(radius=0.3, color=RED).move_to(y.get_center()).shift(RIGHT*0.7)
+        a_label = Tex(r"$a$", color=GREEN).move_to(a_circle.get_center())
+        y_label = Tex(r"$y$", color=RED).move_to(y_circle.get_center())
 
-
+        ay_arrow = Arrow(a_circle.get_center(), y_circle.get_center(), color=GRAY, buff=0.3)
+        ay_label = Tex(r"$h()$", color=GRAY, font_size=36).move_to(ay_arrow.get_center()).shift(UP*0.2)
         self.add(arrows)
         self.add(arrow_labels)
-        self.add(x, y, y_label)
+        self.add(a_circle, a_label, y_circle, y_label)
+        self.add(ay_arrow, ay_label)
+        self.add(x, y)
         self.add(x_labels)
