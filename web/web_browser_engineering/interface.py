c0eb3e1f5c7ae0a1991f246e60f727536409c7c4
iknowca <iknowca@gmail.com>
2025-11-04 10:37:18 +0900

    web_browser_engineering: [ch02]

--- web/web_browser_engineering/interface.py
+++ web/web_browser_engineering/interface.py
@@ -9,14 +9,18 @@ class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+
         self.canvas.pack()
         self.scroll = 0
         self.window.bind("<Down>", self.scrolldown)
         self.window.bind("<Up>", self.scrollup)
+        self.window.bind("<MouseWheel>", self.scrollwheel)
+        self.window.bind("<Configure>", self.expand)
 
     def load(self, url):
         body = url.request()
         text = lex(body)
+        self.text = text
         self.display_list = layout(text)
         self.draw()
 
@@ -25,7 +29,7 @@ class Browser:
         for x, y, c in self.display_list:
             if y > self.scroll + HEIGHT: continue
             if y + VSTEP < self.scroll: continue
-            self.canvas.create_text(x, y - self.scroll, text=c)
+            self.canvas.create_text(x, y - self.scroll, text=c, font=self.bi_times)
 
     def scrolldown(self, e):
         self.scroll += SCROLL_STEP
@@ -35,10 +39,25 @@ class Browser:
         self.scroll -= SCROLL_STEP
         self.draw()
 
+    def scrollwheel(self, e):
+        self.scroll -= e.delta
+        self.draw()
+
+    def expand(self, e):
+        global WIDTH, HEIGHT
+        WIDTH = e.width
+        HEIGHT = e.height
+        self.display_list = layout(self.text)
+        self.draw()
+
 def layout(text):
     display_list = []
     cursor_x, cursor_y = HSTEP, VSTEP
     for c in text:
+        if c == "\n":
+            cursor_x = HSTEP
+            cursor_y += VSTEP
+            continue
         display_list.append((cursor_x, cursor_y, c))
         cursor_x += HSTEP
         if cursor_x >= WIDTH - HSTEP:
