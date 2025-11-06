6a2bffbad2b1ca8bb16275dfe1fa84ebe33b3d36
iknowca <iknowca@gmail.com>
2025-11-06 16:38:56 +0900

    rust/example: file_line_counter -> module

---
+++ language/rust/example/line_counter/src/lib.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+pub fn count() -> usize {
+    let file = File::open("./src/main.rs");
+    println!("file: {:?}", file);
+    let reader = BufReader::new(file.unwrap());
+
+    let mut count = 0;
+    for line in reader.split(b'\n') {
+        let line = line.unwrap();
+        if line.iter().any(|&b| !b.is_ascii_whitespace()) {
+            count += 1;
+        }
+    }
+
+    count
+}
---
+++ language/rust/example/line_counter/src/main.rs
@@ -0,0 +1,8 @@
+use line_counter::count;
+
+
+fn main()  {
+    let count = count();
+    println!("count: {}", count);
+}
+
